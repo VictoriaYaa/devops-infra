875a2833fd27e4c98b53132d7322d603f608c224
VictoriaYaa <victoriaja1916@gmail.com>
2022-05-31 17:07:06 +0300

    changed var and s3 bucket

--- s3-backet.tf
+++ s3-backet.tf
@@ -1,19 +1,21 @@
 #1 -this will create a S3 bucket in AWS
-resource "aws_s3_bucket_versioning" "terraform_state_s3" {
-  bucket = "terraform_state_s3_vic_exam" 
+resource "aws_s3_bucket" "terraform_state_s3" {
+  #make sure you give unique bucket name
+  bucket = "terraform-coachdevops-state" 
   force_destroy = true
-
+# Enable versioning to see full revision history of our state files
   versioning {
          enabled = true
         }
 
-    server_side_encryption_configuration {
-        rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-            }
-        }
+# Enable server-side encryption by default
+server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
+  }
 }
 
 # 2 - this Creates Dynamo Table
