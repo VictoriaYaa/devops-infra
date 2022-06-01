81e607c9b15f927166e1c85cdefdd7e1423f8bd2
VictoriaYaa <victoriaja1916@gmail.com>
2022-06-01 09:57:10 +0300

    added the alb resource

--- security-groups.tf
+++ security-groups.tf
@@ -47,3 +47,39 @@ resource "aws_security_group" "all_worker_mgmt" {
     ]
   }
 }
+
+
+
+# Create Resource Group for the Application Load Balancer
+resource "aws_security_group" "alb-security-group" {
+  name = "vic-alb-sg"
+  description = "Enable HTTP or HTTPS access on Ports 80/443"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTP Access"
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "HTTPS Access"
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+}
