1c87145b73f0147791de388dafccbf7a7dce1351
VictoriaYaa <victoriaja1916@gmail.com>
2022-06-02 09:56:12 +0300

    another commit for the variable

--- output.tf
+++ output.tf
@@ -30,5 +30,5 @@ output "region" {
 
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
-  value       = "${variable.cluster_name}"
+  value       = var.cluster_name
 }
--- vpc.tf
+++ vpc.tf
@@ -16,16 +16,16 @@ module "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    "kubernetes.io/cluster/${variable.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${variable.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/elb"                      = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${variable.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
   }
 }
