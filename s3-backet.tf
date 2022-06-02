dbe42e3a45caf7f77f979a8ec838f30ba84211e3
VictoriaYaa <victoriaja1916@gmail.com>
2022-06-02 09:31:34 +0300

    reverted changes

--- s3-backet.tf
+++ s3-backet.tf
@@ -20,47 +20,6 @@ server_side_encryption_configuration {
 
 
 
-# resource "aws_s3_bucket" "terraform_state_s3" {
-
-#   bucket = "terraform-coachdevops-state" 
-#   force_destroy = true
-# # Enable versioning to see full revision history of our state files
-#   versioning {
-#          enabled = true
-#         }
-
-# # Enable server-side encryption by default
-# server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
-# }
-
-
-# resource "aws_s3_bucket" "terraform_state_s3" {
-
-#   bucket = "terraform-vic-exam" 
-#   force_destroy = true
-# # Enable versioning to see full revision history of our state files
-#   versioning {
-#          enabled = true
-#         }
-
-# # Enable server-side encryption by default
-# server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
-# }
-
-
-
 # 2 - this Creates Dynamo Table
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "tf-up-and-run-locks-vic-NEW"
