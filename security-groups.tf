6f331496c964039a8d7180d8f625ff2e8a093961
VictoriaYaa <victoriaja1916@gmail.com>
2022-06-03 11:46:34 +0300

    removed workers

--- security-groups.tf
+++ security-groups.tf
@@ -1,34 +1,34 @@
-resource "aws_security_group" "worker_group_mgmt_one" {
-  name = "worker_group_mgmt_one_MEW"
-  vpc_id      = module.vpc.vpc_id
+# resource "aws_security_group" "worker_group_mgmt_one" {
+#   name = "worker_group_mgmt_one_MEW"
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+#   ingress {
+#     from_port = 22
+#     to_port   = 22
+#     protocol  = "tcp"
 
-    cidr_blocks = [
-      "10.0.0.0/8",
-      "94.159.136.165/32"
-    ]
-  }
-}
+#     cidr_blocks = [
+#       "10.0.0.0/8",
+#       "94.159.136.165/32"
+#     ]
+#   }
+# }
 
-resource "aws_security_group" "worker_group_mgmt_two" {
-  name = "worker_group_mgmt_two_NEW"
-  vpc_id      = module.vpc.vpc_id
+# resource "aws_security_group" "worker_group_mgmt_two" {
+#   name = "worker_group_mgmt_two_NEW"
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+#   ingress {
+#     from_port = 22
+#     to_port   = 22
+#     protocol  = "tcp"
 
-    cidr_blocks = [
-      "192.168.0.0/16",
-      "94.159.136.165/32"
-    ]
-  }
-}
+#     cidr_blocks = [
+#       "192.168.0.0/16",
+#       "94.159.136.165/32"
+#     ]
+#   }
+# }
 
 resource "aws_security_group" "all_worker_mgmt" {
   name = "all_worker_management_NEW"
