64c54d0d5befaed51d60f5e380515320c6daae2c
VictoriaYaa <victoriaja1916@gmail.com>
2022-06-03 15:31:42 +0300

    changed the eks from module to resource

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,28 +1,126 @@
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "17.24.0"
-  cluster_name    = "${var.cluster_name}"
-  cluster_version = "1.20"
-  subnets         = module.vpc.private_subnets
+resource "aws_iam_role" "EKSClusterRole" {
+  name = "EKSClusterRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
-  vpc_id = module.vpc.vpc_id
 
-  workers_group_defaults = {
-    root_volume_type = "gp2"
+resource "aws_iam_role" "NodeGroupRole" {
+  name = "EKSNodeGroupRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.EKSClusterRole.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.NodeGroupRole.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.NodeGroupRole.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.NodeGroupRole.name
+}
+
+
+
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = "eks-cluster"
+  role_arn = aws_iam_role.EKSClusterRole.arn
+  version  = "1.21"
+
+  vpc_config {
+    subnet_ids          = flatten([ module.vpc.public_subnets_id, module.vpc.private_subnets_id ])
+    security_group_ids  = flatten("${aws_security_group.all_worker_mgmt}")
   }
 
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 1
-      max_capacity     = 1
-      min_capaicty     = 1
-
-      instance_type = "t2.small"
-      } 
-    }
-
-  write_kubeconfig  = true
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy
+  ]
 }
+
+
+resource "aws_eks_node_group" "node-ec2" {
+  cluster_name    = "${var.cluster_name}"
+  node_group_name = "vic-node_group"
+  node_role_arn   = aws_iam_role.NodeGroupRole.arn
+  subnet_ids      = flatten( module.aws_vpc.private_subnets_id )
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  ami_type       = "AL2_x86_64"
+  instance_types = ["t3.micro"]
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
+  ]
+}
+
+
+
+
+# module "eks" {
+#   source          = "terraform-aws-modules/eks/aws"
+#   version         = "17.24.0"
+#   cluster_name    = "${var.cluster_name}"
+#   cluster_version = "1.20"
+#   subnets         = module.vpc.private_subnets
+#   cluster_endpoint_private_access = true
+#   vpc_id = module.vpc.vpc_id
+
+#   node_groups = {
+#     eks_nodes = {
+#       desired_capacity = 1
+#       max_capacity     = 2
+#       min_capaicty     = 1
+#       capacity_type = "ON_DEMAND"
+#       instance_type = "t2.small"
+#       } 
+#     }
+
+#   write_kubeconfig  = true
+# }
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
@@ -31,3 +129,7 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
+
+
+data "aws_availability_zones" "available" {}
+
--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,3 @@
-
-data "aws_availability_zones" "available" {}
-
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
