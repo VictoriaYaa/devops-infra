1e6e6fdb61ee704403baf2bc55281c785ba1140b
VictoriaYaa <victoriaja1916@gmail.com>
2022-06-06 01:56:42 +0300

    another commit

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -79,8 +79,8 @@ resource "aws_eks_node_group" "node-ec2" {
   subnet_ids      = flatten( module.vpc.private_subnets )
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 1
+    max_size     = 10
     min_size     = 1
   }
 
