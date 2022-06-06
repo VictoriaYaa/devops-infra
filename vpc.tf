2c38359d0fb63cfab49467d64a94be1ec7f0778f
VictoriaYaa <victoriaja1916@gmail.com>
2022-06-06 23:47:18 +0300

    changing vpc tags

--- vpc.tf
+++ vpc.tf
@@ -16,12 +16,12 @@ module "vpc" {
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
     "kubernetes.io/role/elb"                      = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
     "kubernetes.io/role/internal-elb"             = 1
   }
 
